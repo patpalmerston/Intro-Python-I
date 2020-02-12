789653e635adeb95de05aa62eca91522586e7649
Pat Palmerston <patpalmerston@gmail.com>
2020-02-11 16:19:28 -0800

    class methods and static methods

--- src/16_class_adv.py
+++ src/16_class_adv.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Employee:
     # class variable / accesses through the class or instance
     raise_amount = 1.04
@@ -8,6 +11,7 @@ class Employee:
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
+        self.email = first + '.' + last + '@email.com'
         self.pay = pay
 
         # using the class reference insted of the self(instance reference) this increments for every instantiation of the class
@@ -23,14 +27,32 @@ class Employee:
     # class Method , alters the method, so now our method takes a class method, insted of the instance (self), can not use the word 'class' so convention is 'cls'
     @classmethod
     def set_raise_amt(cls, amount):
-        pass
+        cls.raise_amount = amount
+
+    # use this class method as alternate constructor to help translate strings into our instance values
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    # creating a static method
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
 emp_1 = Employee('Pat', 'Palmerston', 50000)
 emp_2 = Employee('Leroy', 'Jenkins', 100000)
 
+my_date = datetime.date(2016, 7, 11)
+print(Employee.is_workday(my_date))
+
 # creates raise amount on the emp_1 instance
-emp_1.raise_amount = 1.10
+# emp_1.raise_amount = 1.10
+
+Employee.set_raise_amt(1.05)
 
 # print(Employee.raise_amount)
 # emp_1.apply_raise()
@@ -41,4 +63,14 @@ emp_1.raise_amount = 1.10
 # print(emp_1.__dict__)
 # returns the class dict with the raise amount in it
 # print(Employee.__dict__)
-print(Employee.num_of_emps)
+# print(Employee.num_of_emps)
+
+# Creating a new Employee from a string / But lets make it a class method
+# emp_str_1 = 'John-Doe-60000'
+# new_emp_1 = Employee.from_string(emp_str_1)
+
+# first, last, pay = emp_str_1.split('-')
+# new_emp_1 = Employee(first, last, pay)
+# print(new_emp_1.first)
+# print(new_emp_1.email)
+# print(new_emp_1.pay)
