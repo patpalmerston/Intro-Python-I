507ad42277c60b4c7e35f02d80b652aea08f1166
Pat Palmerston <patpalmerston@gmail.com>
2020-02-11 20:24:38 -0800

    inheritance and attributes

--- src/16_class_adv.py
+++ src/16_class_adv.py
@@ -1,4 +1,7 @@
 import datetime
+'''
+look up the http exception class and import?
+'''
 
 
 class Employee:
@@ -46,21 +49,61 @@ class Employee:
 class Developer(Employee):
     raise_amount = 1.20
 
-    def __init__(self, first, last, pay, prog_lang)
-    super().__init__(first, last, pay)
-    #Employee.__init__(self, first, last, pay)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        #Employee.__init__(self, first, last, pay)
+        self.prog_lang = prog_lang
 
 
-dev_1 = Developer('Pat', 'Palmerston', 50000)
-dev_2 = Developer('Leroy', 'Jenkins', 100000)
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('---->', emp.fullname())
+
+
+dev_1 = Developer('Pat', 'Palmerston', 50000, 'JS')
+dev_2 = Developer('Leroy', 'Jenkins', 100000, 'Python')
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+
+# isinstance will tell us if an object is an instance of a class
+print(isinstance(mgr_1, Manager))  # True
+print(isinstance(mgr_1, Employee))  # True
+print(isinstance(mgr_1, Developer))  # False
+
+# issubclass will tell us if an object is an instance of a subclass
+print(issubclass(Manager, Employee))  # True
+print(issubclass(Manager, Developer))  # False
+
+
+# print(mgr_1.email)
+
+# mgr_1.add_emp(dev_2)
+
+# mgr_1.print_emps()
 
 # print(help(Developer))
 
 # print(dev_1.email)
 # print(dev_2.email)
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
 
 # These are all notes and tests pre sub classes
 
